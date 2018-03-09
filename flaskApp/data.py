f42a09df59a2a09f3e8f4515ae30af190ff81adb
jzztf <zhangtengfei@yandex.com>
2018-03-09 09:30:00 +0800

    initial falskApp

---
+++ flaskApp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'hello world!',
+            'author': 'john',
+            'create_date':'03-06-2018'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'hello world',
+            'author': 'jew',
+            'create_date':'03-07-2018'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'hello world',
+            'author': 'sue',
+            'create_date':'03-08-2018'
+        }
+    ]
+    return articles
